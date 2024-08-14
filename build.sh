6a2ef20c95d3a61ac1dcad8f8a459fa2b4591958
valentin <valentin.rapp@epitech.eu>
2024-08-14 15:59:16 +0200

    added -d for deploy option to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
 bun install
 python3 fix_dependency.py
 bun run build
-echo sexe
+echo "website built"
+
+if [ "$1" = "-d" ]; then
+    sudo cp -rf dist/* /var/www/html
+    echo "website deployed"
+fi
+
